3f5091fc19f448eb6c830e18b2ee34c903d67728
이름 <>
2026-08-05 08:03:21 +0900

    remove vulnerable code

--- public/index.php
+++ public/index.php
@@ -1,17 +1,3 @@
 <?php
-// SQL Injection 취약점
-$id = $_GET['id'];
-$query = "SELECT * FROM users WHERE id = " . $id;
-mysql_query($query);
-
-// XSS 취약점
-echo $_GET['name'];
-
-// 커맨드 인젝션 취약점
-system($_GET['cmd']);
-
-// 파일 인클루드 취약점
-include($_GET['file']);
-
-echo "<h1>Hello from app2 - Security Test</h1>";
+echo "normal text";
 ?>
