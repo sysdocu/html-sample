adfccc25d538ebdde12496749f2eda379e68f851
이름 <>
2026-08-05 10:08:00 +0900

    vulnerable code - scan test step 2

--- public/index.php
+++ public/index.php
@@ -1,4 +1,13 @@
 <?php
-echo "<h1>Hello from app2 - 정상 소스</h1>";
-echo "<p>스캐닝 통과!</p>";
+// SQL Injection 취약점
+$id = $_GET['id'];
+$query = "SELECT * FROM users WHERE id = " . $id;
+
+// XSS 취약점
+echo $_GET['name'];
+
+// 커맨드 인젝션 취약점
+system($_GET['cmd']);
+
+echo "<h1>Hello from app2 - 취약한 소스</h1>";
 ?>
